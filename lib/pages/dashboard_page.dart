9cdd6a8cfcb7b7b6a517e7e4886c7f5920aaec34
Hasan <hsmusayev@gmail.com>
2023-12-30 20:12:40 +0400

    updated dashboard page

--- lib/pages/dashboard_page.dart
+++ lib/pages/dashboard_page.dart
@@ -1,4 +1,6 @@
+import 'package:budget/theme/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_custom_clippers/flutter_custom_clippers.dart';
 
 class DashboardPage extends StatefulWidget {
   const DashboardPage({super.key});
@@ -24,7 +26,10 @@ class _DashboardPageState extends State<DashboardPage> {
           Container(
             height: 350,
             child: Stack(
-              children: [appBarCurve()],
+              children: [
+                appBarCurve(),
+                appBarBalance(),
+              ],
             ),
           )
         ],
@@ -32,5 +37,203 @@ class _DashboardPageState extends State<DashboardPage> {
     );
   }
 
-  Widget appBarCurve() {}
+  Widget appBarCurve() {
+    var size = MediaQuery.of(context).size;
+    return ClipPath(
+      clipper: OvalBottomBorderClipper(),
+      child: Container(
+        width: size.width,
+        height: size.height * 0.25,
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [primary, secondary],
+            ),
+            boxShadow: [
+              BoxShadow(
+                  color: bgColor.withOpacity(0.1),
+                  spreadRadius: 10,
+                  blurRadius: 10)
+            ]),
+        child: Padding(
+          padding: const EdgeInsets.only(top: 15, left: 20, right: 20),
+          child: SafeArea(
+              child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Row(
+                      children: [
+                        Text(
+                          "\$",
+                          style: TextStyle(fontSize: 22, color: white),
+                        ),
+                        Text(
+                          "1.757",
+                          style: TextStyle(fontSize: 30, color: white),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 2,
+                    ),
+                    Text(
+                      "Available balance",
+                      style: TextStyle(color: white.withOpacity(0.8)),
+                    )
+                  ],
+                ),
+                Stack(
+                  children: [
+                    Container(
+                      width: 45,
+                      height: 45,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        image: DecorationImage(
+                            image: NetworkImage(
+                                "https://as2.ftcdn.net/v2/jpg/03/64/21/11/1000_F_364211147_1qgLVxv1Tcq0Ohz3FawUfrtONzz8nq3e.jpg"),
+                            fit: BoxFit.cover),
+                      ),
+                    ),
+                    Positioned(
+                      top: 35,
+                      child: Container(
+                        width: 12,
+                        height: 12,
+                        decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: white,
+                            border: Border.all(color: primary)),
+                      ),
+                    )
+                  ],
+                )
+              ])),
+        ),
+      ),
+    );
+  }
+
+  Widget appBarBalance() {
+    var size = MediaQuery.of(context).size;
+    return Positioned(
+      bottom: 0,
+      left: 0,
+      right: 0,
+      child: Padding(
+        padding: const EdgeInsets.only(left: 20, right: 20),
+        child: Container(
+          width: size.width,
+          height: 200,
+          decoration: BoxDecoration(
+              color: white,
+              borderRadius: BorderRadius.circular(12),
+              boxShadow: [
+                BoxShadow(
+                    color: black.withOpacity(0.01),
+                    spreadRadius: 10,
+                    blurRadius: 10)
+              ]),
+          child: Padding(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Flexible(
+                      child: Row(
+                        children: [
+                          Container(
+                            width: 10,
+                            height: 10,
+                            decoration: BoxDecoration(
+                                color: Colors.redAccent,
+                                shape: BoxShape.circle),
+                          ),
+                          SizedBox(
+                            width: 15,
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "Spent",
+                                style: TextStyle(
+                                  fontSize: 14,
+                                  color: black.withOpacity(0.5),
+                                ),
+                              ),
+                              SizedBox(
+                                height: 3,
+                              ),
+                              Text(
+                                "\$1,460",
+                                style: TextStyle(
+                                    fontSize: 22, fontWeight: FontWeight.w500),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                    Flexible(
+                      child: Row(
+                        children: [
+                          Container(
+                            width: 10,
+                            height: 10,
+                            decoration: BoxDecoration(
+                                color: primary, shape: BoxShape.circle),
+                          ),
+                          SizedBox(
+                            width: 15,
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "Earned",
+                                style: TextStyle(
+                                  fontSize: 14,
+                                  color: black.withOpacity(0.5),
+                                ),
+                              ),
+                              SizedBox(
+                                height: 3,
+                              ),
+                              Text(
+                                "\$2,567",
+                                style: TextStyle(
+                                    fontSize: 22, fontWeight: FontWeight.w500),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                Divider(),
+                SizedBox(
+                  height: 5,
+                ),
+                Text(
+                  "You spent \$1,250 on dining out this month. Let's try to make it lower",
+                  style: TextStyle(fontSize: 14, height: 1.6),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 }
